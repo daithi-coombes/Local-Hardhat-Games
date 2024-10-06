2a4b4f439f87688238e2bfa9248b916c1c26ff9e
daithi <webeire@gmail.com>
2024-10-06 21:07:19 +0300

    win

--- scripts/win.js
+++ scripts/win.js
@@ -1,13 +1,13 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+const contractName = "Game4";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
-    const tx = await game.win();
+    const tx = await game.win(56);
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
